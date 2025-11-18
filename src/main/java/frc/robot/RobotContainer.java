f79b3df9760adc2d2a748efc7c65bb52ec59ef79
Sej5137 <seijiro.dias1@outlook.com>
2025-11-17 16:50:37 -0800

    The rest of the swerve and Vision changes #cat

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -48,21 +48,9 @@ import frc.robot.subsystems.wrist.WristSim;
 import frc.robot.util.Alerts;
 
 public class RobotContainer {
-    private Arm arm;
-    private Elevator elevator;
-    private Wrist wrist;
-    private Hang hang;
-    private Intake intake;
     private Swerve swerve;
 
-    private ArmCommands armCommands;
-    private ElevatorCommands elevatorCommands;
-    private WristCommands wristCommands;
-    private HangCommands hangCommands;
-    private IntakeCommands intakeCommands;
     private SwerveCommands swerveCommands;
-
-    private SuperstructureCommands ssCommands;
 
     // Main drive controller
     private final CommandPS5Controller controller = new CommandPS5Controller(0);
@@ -105,131 +93,18 @@ public class RobotContainer {
         configureAutoChooser();
 
         // Initialize the publisher
-        publisher = new RobotPublisher(arm, wrist, intake, elevator, hang, swerve);
+        publisher = new RobotPublisher(swerve);
     }
 
     private void initSubsystems() {
         // Initialize subsystems in order: arm, elevator, wrist, intake, hang, swerve
         // Each subsystem is created immediately after its motor/encoder initialization
-        if (Constants.armEnabled) {
-            // Initialize arm motor and encoder
-            MotorIO armMotor;
-            EncoderIO armEncoder;
-            switch (Constants.currentMode) {
-                case REAL:
-                case SIM:
-                    armMotor =
-                            new MotorIOTalonFX(Arm.Constants.motorId, Constants.defaultBus, "arm motor", "Arm/Motor");
-                    armEncoder = new EncoderIOCANcoder(
-                            Arm.Constants.encoderId, Constants.defaultBus, "arm encoder", "Arm/Encoder");
-                    break;
-                default:
-                    armMotor = new MotorIO("arm motor", "Arm/Motor");
-                    armEncoder = new EncoderIO("arm encoder", "Arm/Encoder");
-                    break;
-            }
-            // Create arm subsystem
-            arm = new Arm(armMotor, armEncoder);
+        
 
-            if (Constants.currentMode == Mode.SIM) {
-                new ArmSim(armMotor, armEncoder);
-            }
+        
+        
 
-            if (Constants.wristEnabled) {
-                // Initialize wrist motor and encoder
-                MotorIO wristMotor;
-                EncoderIO wristEncoder;
-                switch (Constants.currentMode) {
-                    case REAL:
-                    case SIM:
-                        wristMotor = new MotorIOTalonFX(
-                                Wrist.Constants.motorId, Constants.defaultBus, "wrist motor", "Wrist/Motor");
-                        wristEncoder = new EncoderIOCANcoder(
-                                Wrist.Constants.encoderId, Constants.defaultBus, "wrist encoder", "Wrist/Encoder");
-                        break;
-                    default:
-                        wristMotor = new MotorIO("wrist motor", "Wrist/Motor");
-                        wristEncoder = new EncoderIO("wrist encoder", "Wrist/Encoder");
-                        break;
-                }
-                // Create wrist subsystem
-                wrist = new Wrist(wristMotor, wristEncoder, armMotor);
-
-                if (Constants.currentMode == Mode.SIM) {
-                    new WristSim(wristMotor, wristEncoder);
-                }
-            }
-        }
-
-        if (Constants.elevatorEnabled) {
-            // Initialize elevator motors and encoder
-            MotorIO elevatorLeftMotor;
-            MotorIO elevatorRightMotor;
-            EncoderIO elevatorEncoder;
-            switch (Constants.currentMode) {
-                case REAL:
-                case SIM:
-                    elevatorLeftMotor = new MotorIOTalonFX(
-                            Elevator.Constants.leftMotorId,
-                            Constants.defaultBus,
-                            "elevator left motor",
-                            "Elevator/LeftMotor");
-                    elevatorRightMotor = new MotorIOTalonFX(
-                            Elevator.Constants.rightMotorId,
-                            Constants.defaultBus,
-                            "elevator right motor",
-                            "Elevator/RightMotor");
-                    elevatorEncoder = new EncoderIOCANcoder(
-                            Elevator.Constants.encoderId, Constants.defaultBus, "elevator encoder", "Elevator/Encoder");
-                    break;
-                default:
-                    elevatorLeftMotor = new MotorIO("elevator left motor", "Elevator/LeftMotor");
-                    elevatorRightMotor = new MotorIO("elevator right motor", "Elevator/RightMotor");
-                    elevatorEncoder = new EncoderIO("elevator encoder", "Elevator/Encoder");
-                    break;
-            }
-            // Create elevator subsystem
-            elevator = new Elevator(elevatorLeftMotor, elevatorRightMotor, elevatorEncoder);
-
-            if (Constants.currentMode == Mode.SIM) {
-                new ElevatorSubsystemSim(elevatorLeftMotor, elevatorRightMotor, elevatorEncoder);
-            }
-        }
-
-        if (Constants.intakeEnabled) {
-            // Initialize intake motor
-            MotorIO intakeMotor;
-            switch (Constants.currentMode) {
-                case REAL:
-                case SIM:
-                    intakeMotor = new MotorIOTalonFX(
-                            Intake.Constants.motorId, Constants.defaultBus, "intake motor", "Intake/Motor");
-                    break;
-                default:
-                    intakeMotor = new MotorIO("intake motor", "Intake/Motor");
-                    break;
-            }
-            // Create intake subsystem
-            intake = new Intake(intakeMotor);
-        }
-
-        if (Constants.hangEnabled) {
-            // Initialize hang motor
-            MotorIO hangMotor;
-            switch (Constants.currentMode) {
-                case REAL:
-                case SIM:
-                    hangMotor = new MotorIOTalonFX(
-                            Hang.Constants.motorId, Constants.defaultBus, "hang motor", "Hang/Motor");
-                    break;
-                default:
-                    hangMotor = new MotorIO("hang motor", "Hang/Motor");
-                    break;
-            }
-            // Create hang subsystem
-            hang = new Hang(hangMotor);
-        }
-
+        
         if (Constants.swerveEnabled) {
             // Initialize swerve motors, encoders, and gyro
             MotorIO flDriveMotor, flAngleMotor, frDriveMotor, frAngleMotor;
@@ -366,31 +241,15 @@ public class RobotContainer {
     }
 
     private void initCommands() {
-        armCommands = new ArmCommands(arm);
-        elevatorCommands = new ElevatorCommands(elevator);
-        wristCommands = new WristCommands(wrist);
-        hangCommands = new HangCommands(hang);
-        intakeCommands = new IntakeCommands(intake);
         swerveCommands = new SwerveCommands(swerve);
-        ssCommands = new SuperstructureCommands(armCommands, elevatorCommands, wristCommands);
     }
 
     private void configureBindings() {
         /* ---- Main controller bindings ---- */
-        controller.triangle().onTrue(ssCommands.L4Position());
-        controller.square().onTrue(ssCommands.L3Position());
-        controller.circle().onTrue(ssCommands.L2Position());
-        controller.cross().onTrue(ssCommands.L1Position());
-        controller.options().onTrue(ssCommands.defaultPosition());
+        
 
         // controller.R2().onTrue(ssCommands.lowAlgaePosition());
-        controller.R1().onTrue(ssCommands.sourcePosition());
-
-        controller.L1().onTrue(intakeCommands.intake()).onFalse(intakeCommands.stop());
-        controller.L2().onTrue(intakeCommands.outtake()).onFalse(intakeCommands.stop());
-
-        controller.povUp().onTrue(hangCommands.extendUp()).onFalse(hangCommands.stop());
-        controller.povDown().onTrue(hangCommands.retractDown()).onFalse(hangCommands.stop());
+        
 
         controller.create().onTrue(swerveCommands.resetGyro());
 
@@ -416,11 +275,6 @@ public class RobotContainer {
     private void configureTestBindings() {
         /* ---- Test controller bindings ---- */
         testControllerChooser = new LoggedDashboardChooser<>("Test/Subsystem");
-        testControllerChooser.addOption("Arm", "Arm");
-        testControllerChooser.addOption("Elevator", "Elevator");
-        testControllerChooser.addOption("Wrist", "Wrist");
-        testControllerChooser.addOption("Hang", "Hang");
-        testControllerChooser.addOption("Intake", "Intake");
         testControllerChooser.addOption("Swerve", "Swerve");
 
         testControllerManual = new LoggedDashboardChooser<>("Test/Type");
@@ -439,36 +293,8 @@ public class RobotContainer {
                         () -> Swerve.Constants.swerveFieldCentric.get()));
 
         // Manual duty cycle forward test
-        testController
-                .cross()
-                .and(() -> testControllerManual.get().equals("Manual"))
-                .and(() -> testControllerChooser.get().equals("Arm"))
-                .onTrue(armCommands.setSpeed(0.2))
-                .onFalse(armCommands.stop());
-        testController
-                .cross()
-                .and(() -> testControllerManual.get().equals("Manual"))
-                .and(() -> testControllerChooser.get().equals("Elevator"))
-                .onTrue(elevatorCommands.setSpeed(0.2))
-                .onFalse(elevatorCommands.stop());
-        testController
-                .cross()
-                .and(() -> testControllerManual.get().equals("Manual"))
-                .and(() -> testControllerChooser.get().equals("Wrist"))
-                .onTrue(wristCommands.setSpeed(0.2))
-                .onFalse(wristCommands.stop());
-        testController
-                .cross()
-                .and(() -> testControllerManual.get().equals("Manual"))
-                .and(() -> testControllerChooser.get().equals("Hang"))
-                .onTrue(hangCommands.setSpeed(0.2))
-                .onFalse(hangCommands.stop());
-        testController
-                .cross()
-                .and(() -> testControllerManual.get().equals("Manual"))
-                .and(() -> testControllerChooser.get().equals("Intake"))
-                .onTrue(intakeCommands.setSpeed(0.2))
-                .onFalse(intakeCommands.stop());
+        
+
         testController
                 .cross()
                 .and(() -> testControllerManual.get().equals("Manual"))
@@ -477,36 +303,7 @@ public class RobotContainer {
                 .onFalse(swerveCommands.stop());
 
         // Manual duty cycle backward test
-        testController
-                .circle()
-                .and(() -> testControllerManual.get().equals("Manual"))
-                .and(() -> testControllerChooser.get().equals("Arm"))
-                .onTrue(armCommands.setSpeed(-0.2))
-                .onFalse(armCommands.stop());
-        testController
-                .circle()
-                .and(() -> testControllerManual.get().equals("Manual"))
-                .and(() -> testControllerChooser.get().equals("Elevator"))
-                .onTrue(elevatorCommands.setSpeed(-0.2))
-                .onFalse(elevatorCommands.stop());
-        testController
-                .circle()
-                .and(() -> testControllerManual.get().equals("Manual"))
-                .and(() -> testControllerChooser.get().equals("Wrist"))
-                .onTrue(wristCommands.setSpeed(-0.2))
-                .onFalse(wristCommands.stop());
-        testController
-                .circle()
-                .and(() -> testControllerManual.get().equals("Manual"))
-                .and(() -> testControllerChooser.get().equals("Hang"))
-                .onTrue(hangCommands.setSpeed(-0.2))
-                .onFalse(hangCommands.stop());
-        testController
-                .circle()
-                .and(() -> testControllerManual.get().equals("Manual"))
-                .and(() -> testControllerChooser.get().equals("Intake"))
-                .onTrue(intakeCommands.setSpeed(-0.2))
-                .onFalse(intakeCommands.stop());
+        
         testController
                 .circle()
                 .and(() -> testControllerManual.get().equals("Manual"))
@@ -515,43 +312,14 @@ public class RobotContainer {
                 .onFalse(swerveCommands.stop());
 
         // PID down test
-        testController
-                .cross()
-                .and(() -> testControllerManual.get().equals("PID"))
-                .and(() -> testControllerChooser.get().equals("Arm"))
-                .onTrue(armCommands.setGoal(0));
-        testController
-                .cross()
-                .and(() -> testControllerManual.get().equals("PID"))
-                .and(() -> testControllerChooser.get().equals("Elevator"))
-                .onTrue(elevatorCommands.setGoal(0.1));
-        testController
-                .cross()
-                .and(() -> testControllerManual.get().equals("PID"))
-                .and(() -> testControllerChooser.get().equals("Wrist"))
-                .onTrue(wristCommands.setGoal(0));
+        
         testController
                 .cross()
                 .and(() -> testControllerManual.get().equals("PID"))
                 .and(() -> testControllerChooser.get().equals("Swerve"))
                 .onTrue(swerveCommands.alignToSide(0)); // Align to nearest left reef
 
-        // PID up test
-        testController
-                .circle()
-                .and(() -> testControllerManual.get().equals("PID"))
-                .and(() -> testControllerChooser.get().equals("Arm"))
-                .onTrue(armCommands.setGoal(1));
-        testController
-                .circle()
-                .and(() -> testControllerManual.get().equals("PID"))
-                .and(() -> testControllerChooser.get().equals("Elevator"))
-                .onTrue(elevatorCommands.setGoal(0.5));
-        testController
-                .circle()
-                .and(() -> testControllerManual.get().equals("PID"))
-                .and(() -> testControllerChooser.get().equals("Wrist"))
-                .onTrue(wristCommands.setGoal(1));
+       
         testController
                 .circle()
                 .and(() -> testControllerManual.get().equals("PID"))
@@ -562,26 +330,7 @@ public class RobotContainer {
     // Bindings for manual control of each of the subsystems
     public void configureManualBindings() {
         // Square + circle control arm
-        manualController.square().whileTrue(new RepeatCommand(armCommands.changeGoal(0.05)));
-        manualController.circle().whileTrue(new RepeatCommand(armCommands.changeGoal(-0.05)));
-
-        // Triangle + cross control elevator
-        manualController.triangle().whileTrue(new RepeatCommand(elevatorCommands.changeGoal(0.05)));
-        manualController.cross().whileTrue(new RepeatCommand(elevatorCommands.changeGoal(-0.05)));
-
-        // POV up + down control wrist
-        manualController.povDown().whileTrue(new RepeatCommand(wristCommands.changeGoal(0.05)));
-        manualController.povUp().whileTrue(new RepeatCommand(wristCommands.changeGoal(-0.05)));
-
-        // Intake/outtake controls
-        manualController.L1().onTrue(intakeCommands.intake()).onFalse(intakeCommands.stop());
-        manualController.L2().onTrue(intakeCommands.outtake()).onFalse(intakeCommands.stop());
-
-        // Hang controls
-        manualController.povLeft().onTrue(hangCommands.extendUp()).onFalse(hangCommands.stop());
-        manualController.povRight().onTrue(hangCommands.retractDown()).onFalse(hangCommands.stop());
-
-        manualController.options().onTrue(ssCommands.defaultPosition());
+        
     }
 
     // Refresh drive and manual controller disconnect alerts
@@ -603,19 +352,8 @@ public class RobotContainer {
                     .setPositionOutput(-2, 0)
                     .andThen(new WaitCommand(3))
                     .andThen(swerveCommands.setPositionOutput(0, 0));
-        } else if (autoChooser.get().equals("Left") || autoChooser.get().equals("Right")) {
-            return ssCommands
-                    .defaultPosition()
-                    .andThen(new WaitCommand(0.5))
-                    .andThen(swerveCommands.alignToSide(autoChooser.get().equals("Right") ? 1 : 0))
-                    .andThen(new WaitUntilCommand(
-                            () -> swerve.getRotationError() < 0.1 && swerve.getTranslationError() < 0.1))
-                    .andThen(ssCommands.L4Position())
-                    .andThen(new WaitCommand(3))
-                    .andThen(intakeCommands.outtake())
-                    .andThen(new WaitCommand(0.2))
-                    .andThen(intakeCommands.stop());
-        } else {
+        }
+         else {
             Alerts.create("Unknown auto specified", AlertType.kWarning);
             return new InstantCommand();
         }
--- src/main/java/frc/robot/network/RobotPublisher.java
+++ src/main/java/frc/robot/network/RobotPublisher.java
@@ -19,19 +19,10 @@ import frc.robot.subsystems.wrist.Wrist;
 
 // Class that publishes 3D robot data to AdvantageScope
 public class RobotPublisher {
-    private Arm arm;
-    private Wrist wrist;
-    private Intake intake;
-    private Elevator elevator;
-    private Hang hang;
+  
     private Swerve swerve;
 
-    public RobotPublisher(Arm arm, Wrist wrist, Intake intake, Elevator elevator, Hang hang, Swerve swerve) {
-        this.arm = arm;
-        this.wrist = wrist;
-        this.intake = intake;
-        this.elevator = elevator;
-        this.hang = hang;
+    public RobotPublisher(Swerve swerve) {
         this.swerve = swerve;
     }
 
@@ -48,20 +39,8 @@ public class RobotPublisher {
         // Convert chassisPos to Pose3d, other links need relative positions
         Pose3d mainPos = new Pose3d(chassisPos.getTranslation(), chassisPos.getRotation());
 
-        Pose3d elevatorMiddle =
-                mainPos.plus(new Transform3d(0, 0.0635, elevator.getPosition() / 2 + 0.1, new Rotation3d()));
-        Logger.recordOutput("3DField/ElevatorMiddle", elevatorMiddle);
-
-        Pose3d elevatorInner =
-                elevatorMiddle.plus(new Transform3d(0, 0, elevator.getPosition() / 2 + 0.03, new Rotation3d()));
-        Logger.recordOutput("3DField/ElevatorInner", elevatorInner);
-
+        
         // Subtract pi/2 because the arm was vertical in Fusion
-        Pose3d armPos = elevatorInner.plus(
-                new Transform3d(-0.1143, 0.1284, 0.108, new Rotation3d(0, arm.getPosition() - Math.PI / 2, 0)));
-        Logger.recordOutput("3DField/Arm", armPos);
-
-        Pose3d wristPos = armPos.plus(new Transform3d(0, -0.004, 0.5944, new Rotation3d(0, wrist.getPosition(), 0)));
-        Logger.recordOutput("3DField/Wrist", wristPos);
+        
     }
 }
