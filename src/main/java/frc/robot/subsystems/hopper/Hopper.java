1f3269c34358314021409893380d493f87033751
Jacob <jacobafredrick@gmail.com>
2026-01-19 16:48:13 -0800

    Finished hopper constants, helper functions, and hopper initialization

--- src/main/java/frc/robot/subsystems/hopper/Hopper.java
+++ src/main/java/frc/robot/subsystems/hopper/Hopper.java
@@ -16,9 +16,26 @@ public class Hopper extends SubsystemBase {
 
     public Hopper(MotorIO rollerMotor) {
         this.rollerMotor = rollerMotor;
-        this.rollerMotor.setInverted(false);
-
+        this.rollerMotor.setInverted(Constants.motorInverted);
+        this.rollerMotor.setStatorCurrentLimit(Constants.statorCurrentLimit);
+        this.rollerMotor.setSupplyCurrentLimit(Constants.supplyCurrentLimit);
+        this.rollerMotor.setSupplyCurrentLowerLimit(Constants.supplyCurrentLowerLimit);
+        this.rollerMotor.setSupplyCurrentLowerTime(Constants.supplyCurrentLowerTime);
+        this.rollerMotor.setGearRatio(Constants.gearRatio);
         rollerState = RollerState.ROLLER_STOP;
+    }
+
+    public static class Constants {
+        // CAN device ID for the intake motor controller
+        public static final int motorId = 0;
+        // Whether to flip motor direction (true means reverse forward/backward)
+        public static final boolean motorInverted = false;
+        
+        public static final double statorCurrentLimit = 60; // (amps) limit on motor torque output for intake loads
+        public static final double supplyCurrentLimit = 50; // (amps) normal current limit pulled from battery
+        public static final double supplyCurrentLowerLimit = 35; // (amps) reduce to this if over limit for some time
+        public static final double supplyCurrentLowerTime = 0.3; // (seconds) time before lowering current limit
+        public static final double gearRatio = 1; // Ratio of motor rotations to arm rotations (unitless)
     }
 
     public void setRollerForward() {
