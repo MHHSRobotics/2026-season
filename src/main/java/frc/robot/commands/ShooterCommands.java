b24c742a58540c4f71e9ac48032bac09932c6faf
Sej5137 <seijiro.dias1@outlook.com>
2026-01-21 17:39:27 -0800

    Took out shooter from swerve folder

--- src/main/java/frc/robot/commands/ShooterCommands.java
+++ src/main/java/frc/robot/commands/ShooterCommands.java
@@ -2,8 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-
-import frc.robot.subsystems.swerve.Shooter;
+import frc.robot.subsystems.Shooter;
 
 public class ShooterCommands {
     private Shooter shooter;
