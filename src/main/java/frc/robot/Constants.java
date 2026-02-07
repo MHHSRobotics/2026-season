b5491884e7efef94dd630ec9fb0315aa9ccdf208
Sej5137 <seijiro.dias1@outlook.com>
2026-02-07 14:25:07 -0800

    cat

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -48,10 +48,6 @@ public final class Constants {
 
     // Toggles for susbsytems
     public static final boolean swerveEnabled = true;
-<<<<<<< HEAD
     public static final boolean visionEnabled = true;
     public static final boolean shooterEnabled = true;
-=======
-    public static final boolean visionEnabled = false;
->>>>>>> develop
 }
