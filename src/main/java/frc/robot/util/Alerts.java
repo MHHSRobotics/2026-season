802c351b71b10747792f136d062b747781c6bdfd
npasetto0 <blasphemorgulus@gmail.com>
2026-02-17 10:41:30 -0800

    Fixed duplicated alerts lagging NT

--- src/main/java/frc/robot/util/Alerts.java
+++ src/main/java/frc/robot/util/Alerts.java
@@ -1,13 +1,16 @@
 package frc.robot.util;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 
 // Utility class for one-time alerts
 public class Alerts {
+    private Set<String> dedup=new HashSet<>();
     private List<Alert> alerts = new ArrayList<>();
     private static Alerts alertsObj;
 
@@ -19,9 +22,12 @@ public class Alerts {
     }
 
     public void createAlert(String text, AlertType type) {
-        Alert alert = new Alert(text, type);
-        alert.set(true);
-        alerts.add(alert);
+        if(!dedup.contains(text)){
+            Alert alert = new Alert(text, type);
+            alert.set(true);
+            alerts.add(alert);
+            dedup.add(text);
+        }
     }
 
     // Create a permanent alert with the given text and AlertType (info, warning, or error)
